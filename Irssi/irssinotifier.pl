5429dfd082ab87b5e250c9609a488289fbb71511
Lauri Härsilä <murgo@iki.fi>
2018-11-11 16:46:35 +0200

    Bumped version numbers

--- Irssi/irssinotifier.pl
+++ Irssi/irssinotifier.pl
@@ -15,7 +15,7 @@ use Crypt::GCrypt;
 use Math::Random::ISAAC;
 use vars qw($VERSION %IRSSI);
 
-$VERSION = "22";
+$VERSION = "23";
 %IRSSI   = (
     authors     => "Lauri \'murgo\' Härsilä",
     contact     => "murgo\@iki.fi",
@@ -23,7 +23,7 @@ $VERSION = "22";
     description => "Send notifications about irssi highlights to server",
     license     => "Apache License, version 2.0",
     url         => "https://irssinotifier.appspot.com",
-    changed     => "2017-02-22"
+    changed     => "2018-11-11"
 );
 
 # Sometimes, for some unknown reason, perl emits warnings like the following:
